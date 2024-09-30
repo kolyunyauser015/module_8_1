3e6b8449fa87fa0fd6cc2af880cfe6442e405a52
kolyunyauser015 <89058507+kolyunyauser015@users.noreply.github.com>
2024-09-30 18:36:42 +0600

    Update module_8_1.py

--- module_8_1.py
+++ module_8_1.py
@@ -1,10 +1,10 @@
 def add_everything_up(a, b):
     try:
-        a + b
+        sum = round((a + b), 3)
     except TypeError:
         return str(a) + str(b)
     else:
-        return a + b
+        return sum
 
 print(add_everything_up(123.456, 'строка'))
 print(add_everything_up('яблоко', 4215))
